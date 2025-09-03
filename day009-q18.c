6028f7ac01f62df709923d17a987e14d9f583f97
kush-QoDex-git <kushagra.awasthi.official@gmail.com>
2025-09-03 09:00:29 +0530

    Update day009-q18.c

--- day009-q18.c
+++ day009-q18.c
@@ -1,4 +1,9 @@
-// Q18: Write a program to assign grades based on a percentage input.
+// Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria:
+// 90-100: Grade A
+// 80-89: Grade B
+// 70-79: Grade C
+// 60-69: Grade D
+// below 60: Grade F.
 
 /*
 Sample Test Cases:
@@ -28,22 +33,26 @@ Grade F
 
 int main()
 {
-    float p;
+   float p;
     printf("Enter the percen{tage(out of 100): ");
     scanf("%f", &p);
-    if (p <= 50)
+    if (p < 60)
     {
         printf("Grade F");
     }
-    else if (p <= 68)
+    else if (p >= 60 && p <= 69)
     {
         printf("Grade D");
     }
-    else if (p <= 82)
+    else if (p >= 70 && p <= 79)
+    {
+        printf("Grade C");
+    }
+    else if (p >= 80 && p <= 89)
     {
         printf("Grade B");
     }
-    else if (p <= 100)
+    else if (p >= 90 && p <= 100)
     {
         printf("Grade A");
     }
@@ -54,3 +63,4 @@ int main()
 
     return 0;
 }
+
