534422c2a7a7e853c0c710fcf7b3569141bf4fcf
kush-QoDex-git <kushagra.awasthi.official@gmail.com>
2025-09-16 06:57:17 +0530

    Add files via upload

---
+++ day027-q53.c
@@ -0,0 +1,55 @@
+/*Q53: Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int count =5;
+    
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < ((2*i)+1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for (int i = count-1; i > 0 ; i--)
+    {
+        for (int j = 0; j < ((2*i)-1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
